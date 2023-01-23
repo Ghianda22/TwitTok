8f8d8ead6ca085516a4577fdd685066363c1de35
Valentina Lorè <valelore98@gmail.com>
2023-01-23 10:01:05 +0100

    Created bottom navigation
    Added View Binding to project
    Created navigation views in main activity
    Connected every part to make it work
    Made the app bar sync with the bottom nav

--- app/src/main/java/com/example/twittok/MainActivity.java
+++ app/src/main/java/com/example/twittok/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.twittok;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.twittok.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.fragment_nav_container);
+        AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(R.id.nav_direction_followed, R.id.nav_direction_newtwok, R.id.nav_direction_home).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+
     }
 }
