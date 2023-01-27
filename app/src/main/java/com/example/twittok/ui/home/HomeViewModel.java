9f8ef69bdd75968028273c6149fbe76100da383c
Valentina Lorè <valelore98@gmail.com>
2023-01-27 20:25:09 +0100

    Created view holder & view model with a basic setup

--- app/src/main/java/com/example/twittok/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/twittok/ui/home/HomeViewModel.java
@@ -1,7 +1,19 @@
 package com.example.twittok.ui.home;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.twittok.repositories.TwokRepository;
+
 public class HomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<TwokRepository[]> twokArray = null;
+
+    public MutableLiveData<TwokRepository[]> getUserProfile(){
+        if(twokArray == null){
+            twokArray = new MutableLiveData<TwokRepository[]>();
+
+        }
+        return twokArray;
+    }
+
 }
--- app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewHolder.java
+++ app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewHolder.java
@@ -5,10 +5,18 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.twittok.repositories.TwokRepository;
+
 public class TwokViewHolder extends RecyclerView.ViewHolder {
 
+    //create here all views to be updated in twok_item
 
     public TwokViewHolder(@NonNull View itemView) {
         super(itemView);
+    }
+
+    public void updateContent(TwokRepository twokToShow){
+        //extract all twok properties
+        //assign to views the twok properties
     }
 }
