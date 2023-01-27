edccb47a39e3524fc872ad5947edc3608590b00a
Valentina Lorè <valelore98@gmail.com>
2023-01-27 19:18:08 +0100

    Init files to create twok scroll system
    Installed viewPager2

---
+++ app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewAdapter.java
@@ -0,0 +1,24 @@
+package com.example.twittok.ui.twokscroll;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TwokViewAdapter extends RecyclerView.Adapter<TwokViewHolder> {
+    @NonNull
+    @Override
+    public TwokViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TwokViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.twittok.ui.twokscroll;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TwokViewHolder extends RecyclerView.ViewHolder {
+
+
+    public TwokViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
