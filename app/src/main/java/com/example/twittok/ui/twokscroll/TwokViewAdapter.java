81415b84d8fd04070f924780db4c9fe946bcab51
Valentina Lorè <valelore98@gmail.com>
2023-01-27 20:21:57 +0100

    Created view adapter

--- app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewAdapter.java
+++ app/src/main/java/com/example/twittok/ui/twokscroll/TwokViewAdapter.java
@@ -1,24 +1,43 @@
 package com.example.twittok.ui.twokscroll;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.twittok.R;
+import com.example.twittok.datasource.network.config.ContextSupplier;
+import com.example.twittok.repositories.TwokRepository;
+
+import java.util.List;
+
 public class TwokViewAdapter extends RecyclerView.Adapter<TwokViewHolder> {
+
+    private LayoutInflater inflater = LayoutInflater.from(ContextSupplier.getContext());
+    private List<TwokRepository> twokList;
+
+    public TwokViewAdapter(List<TwokRepository> twokList) {
+        this.twokList = twokList;
+    }
+
     @NonNull
     @Override
     public TwokViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.twok_item, parent, false);
+        return new TwokViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TwokViewHolder holder, int position) {
-
+        TwokRepository twokToShow = twokList.get(position);
+        holder.updateContent(twokToShow);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return twokList.size();
     }
 }
