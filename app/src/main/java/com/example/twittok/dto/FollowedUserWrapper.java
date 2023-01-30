4023d7e25a13662210966d9391770b544eb0a3b9
Valentina Lorè <valelore98@gmail.com>
2023-01-30 10:49:22 +0100

    Created FollowedUserWrapper

--- app/src/main/java/com/example/twittok/dto/FollowedUserWrapper.java
+++ app/src/main/java/com/example/twittok/dto/FollowedUserWrapper.java
@@ -1,5 +1,48 @@
 package com.example.twittok.dto;
 
+import com.example.twittok.datasource.model.UserModel;
+
 public class FollowedUserWrapper {
 
+    // --- ATTRIBUTES ------------------------------------------
+    private UserModel user;
+    private String image;
+
+
+    // --- CONSTRUCTORS ------------------------------------------
+    public FollowedUserWrapper() {
+    }
+    public FollowedUserWrapper(UserModel user, String image) {
+        this.user = user;
+        this.image = image;
+    }
+
+    // --- GETTERS & SETTERS ------------------------------------------
+
+    public UserModel getUser() {
+        return user;
+    }
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+    // --- METHODS ------------------------------------------
+
+
+    @Override
+    public String toString() {
+        return "FollowedUserWrapper{" +
+                "\n    user=" + user +
+                ", \n    image='" + image + '\'' +
+                "\n}";
+    }
 }
+
+
