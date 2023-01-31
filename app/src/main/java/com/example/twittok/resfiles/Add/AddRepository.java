55634d41a5213691531490e7e1d10b72acb24fae
Valentina Lorè <valelore98@gmail.com>
2023-01-31 04:26:47 +0100

    AddTwok Backup

---
+++ app/src/main/java/com/example/twittok/resfiles/Add/AddRepository.java
@@ -0,0 +1,20 @@
+/*
+package com.example.twittok.UI.Add;
+
+import android.util.Log;
+
+import com.example.twittok.Constants;
+import com.example.twittok.Network.ApiObjects.Input.TwokToAdd;
+import com.example.twittok.Network.CommunicationController;
+import com.example.twittok.UI.ListenerInterfaces.OnResponse;
+
+public class AddRepository {
+    public static void addTwok(TwokToAdd twokToAdd, OnResponse onResponse) {
+        CommunicationController.addTwok(
+                twokToAdd,
+                onResponse::giveOutput,
+                t -> Log.d(Constants.ONFAIL, "addTwok call in method addTwok of class AddRepository: " + t.getLocalizedMessage())
+        );
+    }
+}
+*/
